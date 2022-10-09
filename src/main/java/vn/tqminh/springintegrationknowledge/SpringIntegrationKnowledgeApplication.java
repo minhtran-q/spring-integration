656f08c0a4ee44a0716f5349330e53895d6ed978
minh.tranquang <minh.tranquang@nashtechglobal.com>
2022-10-09 17:16:40 +0700

    initialize the sample project

---
+++ src/main/java/vn/tqminh/springintegrationknowledge/SpringIntegrationKnowledgeApplication.java
@@ -0,0 +1,13 @@
+package vn.tqminh.springintegrationknowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationKnowledgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationKnowledgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/tqminh/springintegrationknowledge/SpringIntegrationKnowledgeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.tqminh.springintegrationknowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationKnowledgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
